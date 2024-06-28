39b126f97caf104ab22850d7ebb7f81fa2bdd665
alimagdi25 <alimagdi12367@gmail.com>
2024-06-29 00:24:53 +0300

    first commit

--- controllers/Messages/Messages.controller.js
+++ controllers/Messages/Messages.controller.js
@@ -1,35 +1,40 @@
-
-const Conversation = require('../../models/Conversation/conversation.model');
-
+const Conversation = require("../../models/conversation/conversation.model");
 
 class MessageController {
   constructor(MessageRepository) {
-      this.MessageRepository = MessageRepository;
+    this.MessageRepository = MessageRepository;
   }
 
   async createMessage(senderId, receiverId, content) {
-    const message = await this.MessageRepository.createMessage(senderId, receiverId, content)
-    return message
-}
+    const message = await this.MessageRepository.createMessage(
+      senderId,
+      receiverId,
+      content
+    );
+    return message;
+  }
 
   async getMessagesForUser(req, res) {
-      try {
-        const {sender,receiver } = req.body;
-        const messages = await this.MessageRepository.getMessagesForUser(sender,receiver);
-          res.status(200).json(messages);
-      } catch (error) {
-          res.status(500).json({ error: error.message });
-      }
+    try {
+      const { sender, receiver } = req.body;
+      const messages = await this.MessageRepository.getMessagesForUser(
+        sender,
+        receiver
+      );
+      res.status(200).json(messages);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
   }
 
   async deleteMessage(req, res) {
-      try {
-          const messageId = req.params.messageId;
-          await this.MessageRepository.deleteMessage(messageId);
-          res.status(200).json({ message: 'Message deleted' });
-      } catch (error) {
-          res.status(500).json({ error: error.message });
-      }
+    try {
+      const messageId = req.params.messageId;
+      await this.MessageRepository.deleteMessage(messageId);
+      res.status(200).json({ message: "Message deleted" });
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
   }
 }
 
