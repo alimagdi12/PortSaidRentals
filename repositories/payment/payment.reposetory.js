a17bd6e04596eeb9a3428238db95df7b63357ae3
alimagdi25 <alimagdi12367@gmail.com>
2024-06-29 06:33:29 +0300

    latest commit

--- repositories/payment/payment.reposetory.js
+++ repositories/payment/payment.reposetory.js
@@ -1,48 +1,47 @@
-const orderModel = require("../../models/order/Order.model");
-const Product = require("../../models/products/product.model");
-const User = require("../../models/user/user.model");
+const orderModel = require('../../models/order/order.model');
+const Product = require('../../models/products/product.model');
+const User = require('../../models/user/user.model');
 const jwt = require("jsonwebtoken");
 const Stripe = require("stripe");
-const OrderRepository = require("../order/Order.repository");
+const OrderRepository = require('../order/Order.repository');
 const stripe = Stripe(process.env.STRIPE_KEY);
 
 class PaymentRepository {
-  constructor() {}
+    constructor() { }
 
-  async payment(data, token) {
-    // console.log(token);
-    try {
-      const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
-      if (!decodedToken) return "token must be provided";
+    async payment(data, token) {
+        // console.log(token);
+        try {
+            const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+            if (!decodedToken) return 'token must be provided';
+            
+            const { req } = data;  // Destructure req from data
 
-      const { req } = data; // Destructure req from data
+            const session = await stripe.checkout.sessions.create({
+                payment_method_types: ['card'],
+                line_items: [
+                    {
+                        price_data: {
+                            currency: 'usd',
+                            product_data: {
+                                name: data.name,
+                            },
+                            unit_amount: data.totalPrice * 100, // Stripe expects amount in cents
+                        },
+                        quantity: 1,
+                    },
+                ],
+                mode: 'payment',
+                success_url: `${req.protocol}://${req.get('host')}/checkout-success?success=true&token=${token}`,
+                cancel_url: `${req.protocol}://${req.get('host')}/cart?canceled=true`,
+            });
 
-      const session = await stripe.checkout.sessions.create({
-        payment_method_types: ["card"],
-        line_items: [
-          {
-            price_data: {
-              currency: "usd",
-              product_data: {
-                name: data.name,
-              },
-              unit_amount: data.totalPrice * 100, // Stripe expects amount in cents
-            },
-            quantity: 1,
-          },
-        ],
-        mode: "payment",
-        success_url: `${req.protocol}://${req.get(
-          "host"
-        )}/checkout-success?success=true&token=${token}`,
-        cancel_url: `${req.protocol}://${req.get("host")}/cart?canceled=true`,
-      });
+            return session.url;
 
-      return session.url;
-    } catch (err) {
-      throw err;
+        } catch (err) {
+            throw err;
+        }
     }
-  }
 }
 
 module.exports = PaymentRepository;
